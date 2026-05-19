01fea052d0baf13dc8fe8d365e62f7500c8223ae
Your GitHub Username <your_github_email@example.com>
2026-05-19 23:57:30 +0530

    chore: update build entry point to spa-index.html and add corresponding template file

--- vite.client.config.ts
+++ vite.client.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     outDir: 'dist/client',
     assetsDir: 'assets',
     rollupOptions: {
-      input: ['./index.html'],
+      input: ['./spa-index.html'],
     },
   },
 });
